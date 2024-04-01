05416fa92e19a140a88d755ffb923dda13023370
Benson Trent <bensontrent@gmail.com>
2024-04-01 16:32:19 -0500

    Add support widget

--- theme.config.tsx
+++ theme.config.tsx
@@ -30,6 +30,20 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <script
+dangerouslySetInnerHTML={{
+  __html: `
+  window.fwSettings={
+    'widget_id':154000001780
+    };
+    !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
+`,
+}}
+/>
+<script>
+	
+</script>
+<script type='text/javascript' src='https://widget.freshworks.com/widgets/154000001780.js' async defer></script>
       </>
     ),
     themeSwitch: {
