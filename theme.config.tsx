8b36e1287d8d7fc965f4d0e6ea8df5f5065b304f
Benson Trent <bensontrent@gmail.com>
2024-06-23 10:32:40 -0400

    Update docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -39,6 +39,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="stylesheet"  href="custom-css.css" />
         <script
 dangerouslySetInnerHTML={{
   __html: `
